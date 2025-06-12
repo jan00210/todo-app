d06f041b36d21aba500b79235133b92cdad5936e
janan-i10 <janani10official@gmail.com>
2025-06-12 17:26:58 +0530

    Added Edit and Delete functionality

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class TaskService {
 
+
   private apiUrl = "http://localhost:8080/api/tasks";
 
   constructor(private httpClient: HttpClient) { }
@@ -19,4 +20,11 @@ export class TaskService {
     return this.httpClient.get<Task[]>(this.apiUrl)
   }
 
+  updateTask(taskId: number, updatedTask: Task): Observable<Task> {
+    return this.httpClient.put<Task>(this.apiUrl + '/' + taskId, updatedTask)
+  }
+
+  deleteTask(taskId: number) {
+    return this.httpClient.delete(this.apiUrl + '/' + taskId);
+  }
 }
